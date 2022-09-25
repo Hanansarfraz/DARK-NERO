54d958c8ec3acf1e754f81a87ee4e389de85dbd4
Hanansarfraz <63721759+Hanansarfraz@users.noreply.github.com>
2022-09-25 14:18:31 +0500

    Update config.js

--- config.js
+++ config.js
@@ -36,15 +36,15 @@ global.alivemsg ="HI I'M  ⚜️𝘋𝘈𝘙𝘒 𝘕𝘌𝘙𝘖 𝘔𝘋⚜️
 global.alivepic = "https://i.ibb.co/8N6fR4X/Whats-App-Image-2022-09-17-at-21-22-46.jpg" //you logo key 
 
 
-global.ownername = " KAVEESHA " //ur owner name
-global.owner = ['94774579505','']
-global.premium = ['94774579505','94774579505']
+global.ownername = " HANAN " //ur owner name
+global.owner = ['923432301607','']
+global.premium = ['923432301607','923432301607']
 global.botname = '𝙳⃝𝙰𝚁𝙺 𝙽⃟𝙴𝚁𝙾 𝙼⃝𝙳'
 global.ownername = '𝙳⃝𝙰𝚁𝙺 𝙽⃟𝙴𝚁𝙾 𝙼⃝𝙳'
-global.group1 = 'https://chat.whatsapp.com/GkAmtuJ4jFE6axiNjoSiCX'
-global.group2 = 'https://chat.whatsapp.com/GkAmtuJ4jFE6axiNjoSiCX'
+global.group1 = 'https://chat.whatsapp.com'
+global.group2 = 'https://chat.whatsapp.com'
 global.packname = '𝙳⃝𝙰𝚁𝙺 𝙽⃟𝙴𝚁𝙾 𝙼⃝𝙳'
-global.ytchannel = 'https://youtu.be/B1ri-AFU5hU'
+global.ytchannel = 'https://youtu.bE'
 global.monayawal = '100000'
 global.darahawal = '100'
 global.besiAwal = '15'
